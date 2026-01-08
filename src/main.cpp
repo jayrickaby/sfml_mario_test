25d86a9cfa2331bc4ee3db4f50ab120bc4627dcc
Jay <jayrickaby@pm.me>
2026-01-08 16:46:39 +0000

    v0.1.0-0.0.1 - Semi functioning player - Why is he so jittery?

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
-#include <cstdio>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
 int main(){
     sf::RenderWindow window(sf::VideoMode({600,600}), "Super Mario Bros.");
+
+    // Logic
+    sf::Clock clock;
+    float deltaTime;
+
+    // Player
+    sf::RectangleShape rect({16,16});
+    rect.setFillColor(sf::Color::Red);
+    signed int direction{0};
+    float speed{500.f};
+    sf::Vector2f position{0.f,0.f};
 
     while (window.isOpen()){
         while (const std::optional event = window.pollEvent()){
@@ -11,7 +21,17 @@ int main(){
                 window.close();
             }
         }
+        deltaTime = clock.restart().asSeconds();
+
         window.clear();
+
+        direction = (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) - (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A));
+        std::printf("%i\n", position.x);
+
+        position.x += direction * speed;
+        rect.setPosition(position * deltaTime);
+
+        window.draw(rect);
         window.display();
     }
     return 0;
