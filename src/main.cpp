dd6c3dcc1bdb25d4f65b4000253ba90e83627850
Jay <jayrickaby@pm.me>
2026-01-08 16:26:24 +0000

    v0.1.0 - Window - Window has been added - - It is 600x600 - - It has the title "Super Mario Bros."

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <cstdio>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main(){
-    std::printf("Hello, world!");
+    sf::RenderWindow window(sf::VideoMode({600,600}), "Super Mario Bros.");
 
+    while (window.isOpen()){
+        while (const std::optional event = window.pollEvent()){
+            if (event->is<sf::Event::Closed>()){
+                window.close();
+            }
+        }
+        window.clear();
+        window.display();
+    }
     return 0;
 }
