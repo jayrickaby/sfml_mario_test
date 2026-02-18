311a53548d41115fa127952efd4b18db26be43a3
Jay <jayrickaby@pm.me>
2026-02-18 21:33:14 +0000

    0.13.5a - CMake now split into multiple files. One for src and another for depedencies. - Dependencies are now git submodules rather than FetchContent stuff - Added IMGUI and IMGUI-SFML for future use

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "imgui.h"
+#include "imgui-SFML.h"
+
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <nlohmann/json.hpp>
@@ -6,8 +9,7 @@
 #include "Managers/AnimationManager.h"
 #include "Managers/ModelManager.h"
 #include "Managers/TextureManager.h"
-#include "Types/AnimationSet.h"
-#include "Types/Texture.h"
+#include "spdlog/spdlog.h"
 
 using json = nlohmann::json;
 
@@ -15,24 +17,40 @@ int main(){
     auto screenDimensions = Globals::getScreenDimensions();
     sf::RenderWindow window(sf::VideoMode({600,600}), "Super Mario Bros.");
     sf::View view(sf::Vector2f {screenDimensions.x/2,screenDimensions.y/2}, screenDimensions);
+    ImGui::SFML::Init(window);
 
     AnimationManager::initialise();
     TextureManager::initialise();
     ModelManager::initialise();
+
     Model model = ModelManager::getModel("tiles/questionblock.json");
 
-    while (window.isOpen()){
-        window.setView(view);
+    while (window.isOpen()) {
+        while (const auto event = window.pollEvent()) {
+            ImGui::SFML::ProcessEvent(window, *event);
+
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
 
         Globals::updateDeltaTime();
         model.update();
+        ImGui::SFML::Update(window, sf::seconds(Globals::getDeltaTime()));
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
 
         window.clear();
-
+        window.setView(view);
         model.draw(window);
-
+        window.setView(window.getDefaultView());
+        ImGui::SFML::Render(window);
         window.display();
     }
 
-    return 0;
+    ImGui::SFML::Shutdown();
 }
